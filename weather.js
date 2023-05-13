1edff49ea1f621fb728e47118415a9161a9779ff
Nithin <nithinravindran1990@gmail.com>
2023-05-13 22:49:11 +0530

    weather_style

--- weather.js
+++ weather.js
@@ -5,8 +5,9 @@ function displayweather() {
 }
 
 function displaydata(weatherdetail) {
-  id = weatherdetail.weather[0].id;
-  country = weatherdetail.sys.country;
+  icon=weatherdetail.weather[0].icon
+  ciname = weatherdetail.name;
+  country=weatherdetail.sys.country;
   des = weatherdetail.weather[0].description;
   pressure = Math.round(weatherdetail.main.pressure - 273.15);
   temp = Math.round(weatherdetail.main.temp - 273.15);
@@ -14,15 +15,14 @@ function displaydata(weatherdetail) {
   pressure = weatherdetail.main.pressure
   feels = Math.round(weatherdetail.main.feels_like - 273.15);
 
-
   weatherdata.innerHTML = `<div class="row  mt-2 ms-1 me-1" >
   <div class="card">
     <div class="card-body">
     <h5 style="color:#07f2f2">Weather App</h5>
-    <hr>
+    <img src="./icons/${icon}.png">;
     <h5 class="card-title"><i class="fa-solid fa-temperature-three-quarters"></i> ${temp}°C</h5>
     <h5 class="card-title"><i class="fa-solid fa-cloud"></i> ${des}°C</h5>
-      <h5 class="card-title"><i class="fa-solid fa-location-dot"></i> ${city}, ${country}</h5>
+      <h5 class="card-title"><i class="fa-solid fa-location-dot"></i> ${ciname},${country}</h5>
       <h5 class="card-title"></h5>
       <div class="row">
       <div class="col-6 mt-5" style="border:1px black solid">
